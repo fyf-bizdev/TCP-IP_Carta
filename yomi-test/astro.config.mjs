9f4fa588196cc06534b005346d071ad3c34c327e
y.shimiz <shimizy35@gmail.com>
2024-12-25 04:07:13 +0000

    fix astro.config
    baseの誤りを訂正

--- yomi-test/astro.config.mjs
+++ yomi-test/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://fyf-bizdev.github.io',
-    base: '/TCP_IP-Carta',
+    base: '/TCP-IP_Carta',
 });
