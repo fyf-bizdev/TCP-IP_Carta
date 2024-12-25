5b10f9cfc3587d25825a2252c9ab8dc22f7ba9e6
y.shimiz <shimizy35@gmail.com>
2024-12-25 02:14:26 +0000

    add astro&Actions

---
+++ yomi-test/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://fyf-bizdev.github.io',
+    base: '/TCP_IP-Carta',
+});
