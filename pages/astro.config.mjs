b8db23323ad0b69321d1c04a15994d7a2ba075f8
y.shimiz <shimizy35@gmail.com>
2024-12-27 04:02:03 +0000

    change css framework
    tailwindの追加
    (納会合わせで途中)

--- pages/astro.config.mjs
+++ pages/astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fyf-bizdev.github.io',
   base: '/TCP-IP_Carta',
-  integrations: [svelte()]
+  integrations: [svelte(), tailwind()]
 });
--- pages/src/components/Card.svelte
+++ pages/src/components/Card.svelte
@@ -9,83 +9,70 @@
     const order = [...Array(36)].map((_, i) => i).slice().sort(() => Math.random() - Math.random());
 </script>
 
-<div class="settings">
-    <label class="switch">
-        プロトコル名の表示
-        <input type="checkbox" onclick={() => showName = !showName} checked={showName}>
-        <div class="base round">
-            <span class="slider round"></span>
-        </div>
-    </label>
-    <label class="switch">
-        RFCの表示
-        <input type="checkbox" onclick={() => showRFC = !showRFC} checked={showRFC}>
-        <div class="base round">
-            <span class="slider round"></span>
-        </div>
-    </label>
-    <label class="switch">
-        ポート番号の表示
-        <input type="checkbox" onclick={() => showPort = !showPort} checked={showPort}>
-        <div class="base round">
-            <span class="slider round"></span>
-        </div>
-    </label>
-    <label class="switch">
-        説明の表示
-        <input type="checkbox" onclick={() => showDesc = !showDesc} checked={showDesc}>
-        <div class="base round">
-            <span class="slider round"></span>
-        </div>
-    </label>
-</div>
-
-{#each order as i}
-    <div class="card">
-        <p>{i+1}: </p>
-        {#if showName}
-            <p>プロトコル名: {yomifuda[i].name}</p>
-        {/if}
-        {#if showRFC}
-            <p>RFC: {yomifuda[i].rfc}</p>
-        {/if}
-        {#if showPort}
-            <p>ポート番号: {yomifuda[i].protocol}</p>
-        {/if}
-        {#if showDesc}
-            <p>説明: {yomifuda[i].description}</p>
-        {/if}
+<div class="p-4">
+    <div class="w-2/3 md:w-4/5 flex justify-around items-center flex-col md:flex-row gap-2 mx-auto mb-2 settings
+                [&>label]:cursor-pointer [&>label]:relative [&>label]:flex [&>label]:items-center [&>label]:gap-2
+                [&>label]:md:flex-col [&>label]:md:gap-1
+                [&>label_input[type='checkbox']]:hidden">
+        <label class="switch">
+            プロトコル名の表示
+            <input type="checkbox" onclick={() => showName = !showName} checked={showName}>
+            <div class="base rounded-full">
+                <span class="slider rounded-full"></span>
+            </div>
+        </label>
+        <label class="switch">
+            RFCの表示
+            <input type="checkbox" onclick={() => showRFC = !showRFC} checked={showRFC}>
+            <div class="base rounded-full">
+                <span class="slider rounded-full"></span>
+            </div>
+        </label>
+        <label class="switch">
+            ポート番号の表示
+            <input type="checkbox" onclick={() => showPort = !showPort} checked={showPort}>
+            <div class="base rounded-full">
+                <span class="slider rounded-full"></span>
+            </div>
+        </label>
+        <label class="switch">
+            説明の表示
+            <input type="checkbox" onclick={() => showDesc = !showDesc} checked={showDesc}>
+            <div class="base rounded-full">
+                <span class="slider rounded-full"></span>
+            </div>
+        </label>
     </div>
-    <hr>
-{/each}
+    
+    {#each order as i}
+        <div class="card">
+            <p>{i+1}: </p>
+            {#if showName}
+                <p>プロトコル名: {yomifuda[i].name}</p>
+            {/if}
+            {#if showRFC}
+                <p>RFC: {yomifuda[i].rfc}</p>
+            {/if}
+            {#if showPort}
+                <p>ポート番号: {yomifuda[i].protocol}</p>
+            {/if}
+            {#if showDesc}
+                <p>説明: {yomifuda[i].description}</p>
+            {/if}
+        </div>
+        <hr class="my-2 bg-gray-400 border-0 h-px">
+    {/each}
+</div>
 
 <style>
     .settings {
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        flex-direction: row;
-        margin-bottom: 10px;
-
         --button-width: 50px;
         --button-height: 30px;
     }
 
-    .switch{
-        cursor: pointer;
-        position: relative;
-        display: flex;
-        align-items: center;
-        gap: 10px;
-    }
-
-    .switch input[type="checkbox"] {
-        display: none;
-    }
-
     .base {
         position: relative;
-        background-color: lightgray;
+        background-color: gray;
         transition: background-color 0.2s ease;
         width: var(--button-width);
         height: var(--button-height);
@@ -102,10 +89,6 @@
         height: calc(var(--button-height) - 4px);
         background-color: white;
         transition: transform 0.3s ease;
-    }
-
-    .round {
-        border-radius: calc(infinity * 1px);
     }
 
     .switch input[type="checkbox"]:checked + .base .slider {
